71f25f13dd0ad6c1e960365cfba00c9e471e6e14
Dyuthi345 <149928902+Dyuthi345@users.noreply.github.com>
2023-11-05 14:22:18 +0530

    Add project files.

---
+++ valid_date/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a string");
+        string input = Console.ReadLine();
+
+        // Regular expression used to find valid dates in mmddyyyy format
+        var dateMatches = FindValidDates(input); 
+        if (dateMatches.Count > 0)
+        {
+            Console.WriteLine("Valid Dates found in the string");
+            foreach (var date in dateMatches)
+            {
+                Console.WriteLine(date);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No Vaild dates found in the string");
+        }
+    }
+     static MatchCollection FindValidDates(string input)
+    {
+        // Regular expression Defined for mmddyyyy format
+        string pattern = @"\b(\d{2})(\d{2})(\d{4})\b"; 
+
+        // Regular Expression used to find matches in input string
+        Regex regex = new Regex(pattern);
+        MatchCollection matches = regex.Matches(input);
+        return matches;
+    }
+}
